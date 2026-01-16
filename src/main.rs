e3a22d71ddd897cf554689ee2408f2dd558ab2c2
buave <bauviane@protonmail.com>
2026-01-16 16:33:57 +0100

    Change method of generate noise

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ use axum::{
     routing::get,
     Router,
 };
-use rand::{distributions::Alphanumeric, Rng};
+use base64;
+use base64::{engine::general_purpose, Engine as _};
 use std::{env, path::PathBuf};
 use tokio::fs;
 
@@ -26,22 +27,34 @@ async fn handle_request(Path(filename): Path<String>) -> impl IntoResponse {
     } else {
         Response::builder()
             .status(StatusCode::OK)
-            .body(axum::body::Body::from(generate_random_string()))
+            .body(axum::body::Body::from(generate_random_string(filename)))
             .unwrap()
     }
 }
 
 async fn index() -> impl IntoResponse {
-    generate_random_string()
+    generate_random_string("index".to_string())
 }
 
-fn generate_random_string() -> String {
-    let size = rand::thread_rng().gen_range(50..1000);
-    rand::thread_rng()
-        .sample_iter(&Alphanumeric)
-        .take(size)
-        .map(char::from)
-        .collect()
+fn generate_random_string(value: String) -> String {
+    let size = ((value.len() + 67) * 3) / 2;
+    let mut base64 = general_purpose::STANDARD.encode(value);
+    for _ in 0..14 {
+        base64 = general_purpose::STANDARD.encode(base64);
+    }
+
+    let cut_base64 = &base64[..size];
+    let mut chars: Vec<char> = cut_base64.chars().collect();
+    let len_base64 = cut_base64.len();
+
+    for r in 0..(size / 2) {
+        for i in 0..len_base64 {
+            let j = (i + r + 3) % len_base64;
+            chars.swap(i, j);
+        }
+    }
+
+    format!("{}\n", String::from_iter(chars))
 }
 
 #[tokio::main]
